25f8ae8333c2c9466ab965fc48e573ae57a6e17e
Tiago Alves <tiagoalves@Tgo.localdomain>
2025-05-20 15:18:06 -0300

    sheets.py final com escopos corrigidos

--- sheets.py
+++ sheets.py
@@ -4,16 +4,23 @@ import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime
 
-# Credenciais e escopos
+# Carrega credenciais do JSON da variável de ambiente
 creds_dict = json.loads(os.getenv("GOOGLE_CREDS_JSON"))
-scopes = ['https://www.googleapis.com/auth/spreadsheets']
-creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
 
-# Autenticação e planilha
+# Define escopos corretos (sheets + drive)
+scopes = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+]
+
+# Autenticação com Google
+creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
 gc = gspread.authorize(creds)
+
+# Abre a planilha e aba
 sheet = gc.open(os.getenv("GOOGLE_SHEET_NAME")).worksheet("Mensagens")
 
-# Função de escrita
+# Função de gravação no Google Sheets
 def salvar_no_sheets(data, numero, original, topicos, plano):
     sheet.append_row([
         data.strftime('%d/%m/%Y %H:%M'),
