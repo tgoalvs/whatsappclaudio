7b1abdb05ad24143481a42cc7e4044c25ebfe848
Tiago Alves <tiagoalves@Tgo.localdomain>
2025-05-20 15:14:53 -0300

    Adicionado escopo da API do Google Sheets

--- sheets.py
+++ sheets.py
@@ -4,17 +4,16 @@ import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime
 
-# Carrega credenciais do JSON vindo da variável de ambiente
+# Credenciais e escopos
 creds_dict = json.loads(os.getenv("GOOGLE_CREDS_JSON"))
-creds = Credentials.from_service_account_info(creds_dict)
+scopes = ['https://www.googleapis.com/auth/spreadsheets']
+creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
 
-# Autentica com o Google Sheets
+# Autenticação e planilha
 gc = gspread.authorize(creds)
-
-# Abre a planilha e aba
 sheet = gc.open(os.getenv("GOOGLE_SHEET_NAME")).worksheet("Mensagens")
 
-# Função de salvamento
+# Função de escrita
 def salvar_no_sheets(data, numero, original, topicos, plano):
     sheet.append_row([
         data.strftime('%d/%m/%Y %H:%M'),
